6f9b0fb0d4bb61618e0427925ab2138b68c51074
zhangdd_ewan <zhangdongdong50@huawei.com>
2024-04-15 14:08:14 +0800

    abseil组件没升级导致编译问题
    Signed-off-by: zhangdd_ewan <zhangdongdong50@huawei.com>

--- cpp/src/phonenumbers/geocoding/phonenumber_offline_geocoder.cc
+++ cpp/src/phonenumbers/geocoding/phonenumber_offline_geocoder.cc
@@ -27,7 +27,6 @@
 #include "phonenumbers/phonenumberutil.h"
 #include "phonenumbers/stl_util.h"
 
-#include "absl/synchronization/mutex.h"
 
 namespace i18n {
 namespace phonenumbers {
@@ -77,7 +76,6 @@ void PhoneNumberOfflineGeocoder::Init(
 }
 
 PhoneNumberOfflineGeocoder::~PhoneNumberOfflineGeocoder() {
-  absl::MutexLock l(&mu_);
   gtl::STLDeleteContainerPairSecondPointers(
       available_maps_.begin(), available_maps_.end());
 }
@@ -195,7 +193,6 @@ const char* PhoneNumberOfflineGeocoder::GetAreaDescription(
   const int country_calling_code = number.country_code();
   // NANPA area is not split in C++ code.
   const int phone_prefix = country_calling_code;
-  absl::MutexLock l(&mu_);
   const AreaCodeMap* const descriptions = GetPhonePrefixDescriptions(
       phone_prefix, lang, script, region);
   const char* description = descriptions ? descriptions->Lookup(number) : NULL;
