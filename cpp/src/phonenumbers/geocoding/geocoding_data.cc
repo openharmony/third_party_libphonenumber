04940ec87cd513b2b6ebb3b19c8c272fabc3077a
zhangdd_ewan <zhangdongdong50@huawei.com>
2026-01-22 10:45:37 +0800

    修改吉林市号码归属地
    Signed-off-by: zhangdd_ewan <zhangdongdong50@huawei.com>

--- cpp/src/phonenumbers/geocoding/geocoding_data.cc
+++ cpp/src/phonenumbers/geocoding/geocoding_data.cc
@@ -1,4 +1,4 @@
 # This file was originally tracked by Git LFS but LFS tracking has been removed
 version https://git-lfs.github.com/spec/v1
-oid sha256:13ab99aa48793a0dea60cb96c2bbe1bd840a0457d59c6a35eaba7cc6f32a6dea
-size 28661035
+oid sha256:681ec3fc08da2f0767a464a4d1586287f0293dc799008fc4684c1431ca967078
+size 28663286
