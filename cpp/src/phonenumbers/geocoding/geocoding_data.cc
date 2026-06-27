7e1c564b8c0feafa2fd17cb21a337a60e065838c
zhangdd_ewan <zhangdongdong50@huawei.com>
2026-06-27 20:25:31 +0800

    optimize rom
    Signed-off-by: zhangdd_ewan <zhangdongdong50@huawei.com>

--- cpp/src/phonenumbers/geocoding/geocoding_data.cc
+++ cpp/src/phonenumbers/geocoding/geocoding_data.cc
@@ -1,4 +1,4 @@
 # This file was originally tracked by Git LFS but LFS tracking has been removed
 version https://git-lfs.github.com/spec/v1
-oid sha256:e5096293ab6abd1bfc9162095cc1d7642930072879c124d470f56ab79bc8cb7a
-size 10472506
+oid sha256:d1fbefbe1fb8837cd2aaa19a90550b2c31aa1061d661d8d79657e58d4dafa11b
+size 10472496
